3b8a6d5ada702a3b2fe3f7cd7cc2365a0874457b
rebecca <joe.miehl@gmail.com>
2023-08-19 08:04:40 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Joe Miehl on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
